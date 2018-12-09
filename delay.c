9a418aeee4a6eed40f09996d94fc839d863841de
Jan Mrazek <mrazeja7@fel.cvut.cz>
2018-12-09 18:24:46 +0100

    remove unused delay functions

--- delay.c
+++ delay.c
@@ -1,27 +1,15 @@
 #include "stm32f4xx_conf.h"
 #include "delay.h"
-void Delay(__IO uint32_t nTime)
-{ 
-    uwTimingDelay = nTime;
-
-    while(uwTimingDelay-- != 0);
-}
-
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-    if (uwTimingDelay != 0x00)
-    { 
-        uwTimingDelay--;
-    }
-}
+/* code lifted from the LCD example. It's bad but it works. */
 
 void busyDelay()
 {   
     for (int i=0; i<150; i++)
+        for (int j=0; j<30000; j++);
+}
+
+void shortDelay()
+{   
+    for (int i=0; i<50; i++)
         for (int j=0; j<30000; j++);
 }
